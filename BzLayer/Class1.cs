e479fa7ab6558b8399f106e4250a44a825d957a0
SameerJ14 <sameerjawale582@gmail.com>
2023-10-20 11:40:36 +0000

    first commit

---
+++ BzLayer/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Entity;
+using DataLayer;
+namespace BzLayer;
+
+public class MovieBz
+
+{
+
+public List<Movie> GetMovies(){
+   DataAccess d = new DataAccess();
+    return d.GetMovies();
+}
+
+
+}
